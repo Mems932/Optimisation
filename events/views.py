d9cd8403a1f1d91270b2b23de959a1f48a911130
Mostapha <mostapha-bachir.addi@redstoneformations.fr>
2024-11-20 14:33:32 +0100

    Ajout filtre de recherche

--- events/views.py
+++ events/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .serializers import EventSerializer, ParticipantSerializer
 from .models import Event, Participant
+
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'location']
+
 
 class ParticipantViewSet(viewsets.ModelViewSet):
     queryset = Participant.objects.all()
